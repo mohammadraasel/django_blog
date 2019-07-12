1abdcacba972d33910dd3d454289da366974cd21
Mohammmd Rasel <mohammadrasel1221@gmail.com>
2019-07-13 00:33:01 +0600

    customize register form

--- users/views.py
+++ users/views.py
@@ -1,16 +1,18 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from .forms import UserRegisterForm
+from django.contrib import messages
 
 # Create your views here.
 
 def register(request):
     if request.method == "POST":
-        form = UserCreationForm(request.POST)
+        form = UserRegisterForm(request.POST)
         if form.is_valid():
             form.save()
+            messages.success(request, f"Your account has been created!")
             return redirect('blog_home')
     else:
-        form = UserCreationForm()
+        form = UserRegisterForm()
 
     return render(request, 'users/register.html', {'form': form})
 
