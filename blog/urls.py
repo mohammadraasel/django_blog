647e34842f2cef5e90b564a636080f172ba432c1
unknown <rana.akash0003@gmail.com>
2019-07-18 23:10:25 +0600

    post details and pagination added

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,9 @@
-
 from django.urls import path
 from . import views
 
 
 urlpatterns = [
-    path('', views.home, name ='blog_home'),
-    path('about/', views.about, name ='blog_about')
+    path('', views.home, name='blog_home'),
+    path('post-detail/<int:post_id>/', views.PostDetail, name='post_detail'),
+    path('about/', views.about, name='blog_about'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,19 +1,29 @@
 from django.shortcuts import render
 from .models import Post
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 
 
 # Create your views here.
 
 
-
-
-
 def home(request):
+    posts = Post.objects.order_by('created_at')
+    paginator = Paginator(posts, 3)
+    page = request.GET.get('page')
+    paged_posts = paginator.get_page(page)
     context = {
-        "posts": Post.objects.all()
+        "posts": paged_posts
     }
     return render(request, 'blog/home.html', context)
 
 
+def PostDetail(request, post_id):
+    posts = Post.objects.filter(pk=post_id)
+    posts ={
+        'posts' : posts
+    }
+    return render(request, 'blog/post/post.html', posts)
+
+
 def about(request):
-    return render(request, 'blog/about.html', {'title':"About"})
+    return render(request, 'blog/about.html', {'title': "About"})
