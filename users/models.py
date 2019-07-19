addc930e1a802dad5b36ea48f3de01ef94592eaf
Mohammmd Rasel <mohammadrasel1221@gmail.com>
2019-07-20 01:11:41 +0600

    modify update user profile

--- users/models.py
+++ users/models.py
@@ -12,11 +12,11 @@ class Profile(models.Model):
     def save(self):
         super().save()
 
-        img = Image.open(self.image.path)
-        if img.height > 300 or img.width > 300:
+        image = Image.open(self.image.path)
+        if image.height > 300 or image.width > 300:
             output_size = (300, 300)
-            img.thumbnail(output_size)
-            img.save(self.image.path)
+            image.thumbnail(output_size)
+            image.save(self.image.path)
 
 
 
