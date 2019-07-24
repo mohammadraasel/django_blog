08ad0764d7e487754d0f8c365b27c9f7c688e91f
MD TANVIR AHMED <38840252+tanvirewu@users.noreply.github.com>
2019-07-24 23:44:12 +0600

    New post navigation link added

--- django_blog/settings.py
+++ django_blog/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'blog',
         'USER': 'postgres',
-        'PASSWORD': 'root',
+        'PASSWORD': 'mnbvcxz',
         'HOST': 'localhost',
         'PORT': '5432',
     }
